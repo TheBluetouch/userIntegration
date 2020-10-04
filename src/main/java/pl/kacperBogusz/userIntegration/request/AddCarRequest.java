b5d138727ba7477fb6e2c816dd24e038f1a3ecb7
Bluetouch <bogusz28@wp.pl>
2020-10-04 15:52:50 +0200

    third commit

---
+++ src/main/java/pl/kacperBogusz/userIntegration/request/AddCarRequest.java
@@ -0,0 +1,10 @@
+package pl.kacperBogusz.userIntegration.request;
+
+import lombok.Data;
+
+@Data
+public class AddCarRequest {
+    private String model;
+    private String company;
+    private int year;
+}
